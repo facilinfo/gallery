57e8cd6a02267fe87d00973e7daf237347fb86d6
facilinfo <you@example.com>
2016-01-10 15:57:27 +0000

    added config views-templates

--- src/Config/gallery.php
+++ src/Config/gallery.php
@@ -2,6 +2,7 @@
 
 return [
     'path' => '/uploads/gallery/', //(in public)
+    'views-template' => 'gallery.app',
     'categories-view' => 'gallery.categories.public-index',
     'series-view' => 'gallery.series.public-index',
     'images-view' => 'gallery.images.public-index'
